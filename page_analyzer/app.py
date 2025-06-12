a9a0a536f58779ca089dff8b9330b7c23ceaab91
Сергей Князьков <knyazkovsv@rushydro.ru>
2025-06-12 21:01:29 +0400

    Добавлена переменная окружения SECRET_KEY
    (cherry picked from commit 39e81f6bcc8137886997643e4abca70fcabd74c5)

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
 @app.route('/')
